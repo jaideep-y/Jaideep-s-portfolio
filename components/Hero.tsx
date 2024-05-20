dd43d56580ee075cf77686f923cfa631d0bab97f
Jaideep Yogish <jaideepyogish@Jaideeps-MacBook-Air.local>
2024-05-20 09:52:24 +0530

    first commit

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -6,7 +6,7 @@ import { TextGenerateEffect } from "./ui/TextGenerateEffect";
 
 const Hero = () => {
   return (
-    <div className="pb-20 pt-36">
+    <div className="pb-20 pt-36" id="hero">
       {/**
        *  UI: Spotlights
        *  Link: https://ui.aceternity.com/components/spotlight
@@ -57,7 +57,7 @@ const Hero = () => {
           />
 
           <p className="text-center md:tracking-wider mb-4 text-sm md:text-lg lg:text-2xl">
-            Hi! I&apos;m Adrian, a Next.js Developer based in Croatia.
+            Hi! I&apos;m Jaideep, a Next.js Developer.
           </p>
 
           <a href="#about">
